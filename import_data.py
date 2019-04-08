c64308f9f06508397d4d8a43f6eb7e17d32c2b61
Gulomjon Saidov <gulomjonsaidov@gmail.com>
2019-04-08 07:54:47 -0700

    importing function

---
+++ import_data.py
@@ -0,0 +1,12 @@
+import json
+import geopandas as gpd
+import numpy as np
+
+
+# Import necessary data in geopandas format
+
+def get_geo_data(filepath):
+    df = gpd.read_file(filepath)
+
+    return df # returns a geopandas dataframe
+
