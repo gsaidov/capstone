a1467f8683d3fd1ae8ca3dae49310446aeb961c7
Gulomjon Saidov <gulomjonsaidov@gmail.com>
2019-04-11 15:18:32 -0700

    comments added

--- src/model.py
+++ src/model.py
@@ -7,25 +7,26 @@ from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 
-
+# Running a linear regression model
 def linear(X, y):
+    # First split the data
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=3)
 
     model = LinearRegression().fit(X_train, y_train)
-    return mean_squared_error(y_test, model.predict(X_test))
+    return mean_squared_error(y_test, model.predict(X_test))**0.5 # Root Mean Squared Error
 
 
-
+# Running a random forest regressor model
 def random_forest(X, y):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=3)
 
     model = RandomForestRegressor().fit(X_train, y_train)
-    return mean_squared_error(y_test, model.predict(X_test))
+    return mean_squared_error(y_test, model.predict(X_test))**0.5 # RMSE
 
-
+# Running a gradient boosting regressor model
 def grad_boost(X, y):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=3)
 
     model = GradientBoostingRegressor().fit(X_train, y_train)
-    return mean_squared_error(y_test, model.predict(X_test))
+    return mean_squared_error(y_test, model.predict(X_test))**0.5 # RMSE
 
