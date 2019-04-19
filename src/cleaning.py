4ce9fc24fcb9e98eaa404cf9e8ea7ec6f175e0d3
Gulomjon Saidov <gulomjonsaidov@gmail.com>
2019-04-19 09:53:06 -0700

    fixed time function

--- src/cleaning.py
+++ src/cleaning.py
@@ -8,7 +8,7 @@ import numpy as np
 def convert_date(df, column):
     # df is the pandas or geopandas dataframe
     # column is the name of the column (string)
-    df.loc[:, 'new_column_name'] = pd.to_datetime(df[column])
+    return df.loc[:, 'new_column_name'] = pd.to_datetime(df[column])
 
 
 # Create a year, month, day, and dayofweek columns
@@ -17,7 +17,7 @@ def create_time(df, , date_col, length_of_time):
     # df is the pandas or geopandas dataframe
     # length_of_time is the name of time length (string) : year, month, day, dayofweek
     # date_col is the datetime column of the dataframe (string)
-    new_df.loc[:, length_of_time] = pd.DatetimeIndex(new_df[date_col]).length_of_time
+    return df.loc[:, length_of_time] = pd.DatetimeIndex(df[date_col]).length_of_time
 
 
 
